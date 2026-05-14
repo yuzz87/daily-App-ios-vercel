96052186aaf36d2a2259efd19ff3f5ca7074f48c
Yamamoto 87 <cs@gmail.com>
2026-05-15 03:17:51 +0900

    change miss

--- mastra/src/workflows/snapNoteWorkflow.ts
+++
@@ -1,9 +0,0 @@
-import { analyzeSnapNote } from "../agents/snapNoteAgent.js";
-import type { SnapNoteAnalysisInput, SnapNoteAnalysisOutput } from "../schemas/snapNote.js";
-
-export async function runSnapNoteWorkflow(
-  input: SnapNoteAnalysisInput,
-): Promise<SnapNoteAnalysisOutput> {
-  return analyzeSnapNote(input);
-}
-
